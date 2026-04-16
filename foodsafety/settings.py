4ebdd22c5ac4a499d3f9af95b775333febba739a
Inba-kgm <subramanimani2080@gmail.com>
2026-04-17 03:19:30 +0530

    Remove duplicate middleware entries
    Removed duplicate middleware entries for SecurityMiddleware and WhiteNoiseMiddleware.

--- foodsafety/settings.py
+++ foodsafety/settings.py
@@ -43,15 +43,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+
 ]
 
 ROOT_URLCONF = 'foodsafety.urls'
