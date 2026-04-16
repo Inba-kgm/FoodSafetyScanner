09f0f61720ca5048e08a651fdb5ae03145ddfba5
Inba-kgm <subramanimani2080@gmail.com>
2026-04-17 03:09:41 +0530

    Add WhiteNoise middleware for static file handling

--- foodsafety/wsgi.py
+++ foodsafety/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodsafety.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles'))
